4803b0e06958b0577fd140dcf733e8e9e50eb21a
saurabh chauhan <sourabh1202@gmail.com>
2025-08-04 20:07:24 +0300

    removed config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,14 +5,14 @@ import type { NextConfig } from "next";
 console.log(`${process.env.NEXT_PUBLIC_BASE_URL}/api/user-chats`);
 const nextConfig: NextConfig = {
   compress:false,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/api/:path*`, // dynamic from env
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: `${process.env.NEXT_PUBLIC_BASE_URL}/api/:path*`, // dynamic from env
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
